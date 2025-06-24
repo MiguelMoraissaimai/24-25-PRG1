430a89d15b8e15c42ea0a0c586827f6a9939f600
Miugel Justdance <joao.morais@alumnos.uneatlantico.es>
2025-06-24 19:33:50 +0200

    string equality

--- entregas/moraisMiguel/java/JuegosFor.java
+++ entregas/moraisMiguel/java/JuegosFor.java
@@ -1,12 +1,22 @@
+import java.util.Scanner;
+
 class JuegosFor{
     public static void main(String[] args){
 
-        for(int i=0;i<10;i++){
-            for(int a=0;a<20;a++){
-                System.out.println("x".repeat(i));
+        Scanner input = new Scanner(System.in);
+
+        String text = input.nextLine();
+
+        if(text.equals("sigma")){
+
+            for(int i=0;i<10;i++){
+                for(int a=0;a<20;a++){
+                    System.out.println("x".repeat(i));
+                }
+                System.out.println(i);
             }
-            System.out.println(i);
-        }
+
+    }
 
     }
 }
